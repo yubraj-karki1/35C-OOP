b74f080c4c9336299bf3857188da5840fb6634e1
yubraj <yubraj.yubrajkarki188@gmail.com>
2024-04-12 12:36:15 +0545

    Week 1 Question 1

--- Week 1/Hello.java
+++ Week 1/Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args) {
+        /* Write a program that prints Hello World! to the console. Save the program as Hello.java */
         System.out.println("Hello world!");
     }
 }
